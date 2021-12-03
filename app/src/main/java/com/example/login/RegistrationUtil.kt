7edfbffbc95cdf64c7328abfb445508ec7dfd490
TerrySong <tisntarry@gmail.com>
2021-12-03 10:01:05 -0800

    tests passed

--- app/src/main/java/com/example/login/RegistrationUtil.kt
+++ app/src/main/java/com/example/login/RegistrationUtil.kt
@@ -2,34 +2,72 @@ package com.example.login
 
 //object keyword makes it so all the functions are static functions
 object RegistrationUtil {
+    var existingUsers = listOf("bigboi", "asdf", ": ^)")
+    var existingEmails = listOf("connersmith1254@gmail.com", "therock@byahoo.com")
+    var forbiddenWords = listOf("mushroom")
+    var name = ""
 
     //isn't empty
     //isn't already used
     //correct format
     fun validateEmail(email: String) : Boolean {
-        return true
+        if(email != "" && !existingEmails.contains(email)){
+            if (email.substring(email.length - 4) == ".com" && email.contains("@"))
+                return true
+        }
+        return false
     }
 
+    //only letters
     fun validateName(name: String) : Boolean {
+        val chars = name.toCharArray()
+
+        for (c in chars) {
+            if (!Character.isLetter(c)) {
+                return false
+            }
+        }
+        this.name = name
         return true
     }
 
     //forbidden words
     //already taken
     //can't haven real name
-    //min of characters
-    //other requirements: capital letters, etc
+    //maximum 30 Min 1
     fun validateUsername(username: String) : Boolean {
-        return true
+        if(!existingUsers.contains(username) && !username.contains(name) && username.isNotEmpty() && username.length <= 32){
+            if(username.indexOfAny(forbiddenWords) == -1)
+                return true
+        }
+        return false
     }
 
     //not empty
     //meets security requirements: at least 8 chars, 1 digit, 1 cap letters
     //passwords match
     fun validatePassword(password: String, confirmPassword : String) : Boolean {
-        if(password == "" || confirmPassword == ""){
-            return false
-        }else if(password.length < 8 || password. )
+        if (password != "" && password.length >= 8) {
+            if (password.indexOfAny(
+                    charArrayOf(
+                        'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K',
+                        'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X',
+                        'Y', 'Z'
+                    )
+                ) >= 0
+            ) {
+                if (password.indexOfAny(
+                        charArrayOf(
+                            '0', '1', '2', '3', '4', '5', '6', '7', '8',
+                            '9'
+                        )
+                    ) >= 0
+                ){
+                    if(password == confirmPassword)
+                        return true
+                }
+            }
+        }
+        return false
     }
-
 }
--- app/src/test/java/com/example/login/RegistrationUtilTest.kt
+++ app/src/test/java/com/example/login/RegistrationUtilTest.kt
@@ -14,25 +14,105 @@ class RegistrationUtilTest {
 
     @Test
     fun validatePassword_passwordsDontMatch_returnFalse(){
-        val result = RegistrationUtil.validatePassword("123", "132")
+        val result = RegistrationUtil.validatePassword("123dfsdf", "132asdfa")
         assertThat(result).isFalse()
     }
 
     @Test
-    fun validatePassword_passwordContains8Char_returnTrue(){
-        val result = RegistrationUtil.validatePassword("12345678", "12345678")
-        assertThat(result).isTrue()
+    fun validatePassword_passwordContains8Char_returnFalse(){
+        val result = RegistrationUtil.validatePassword("1345678", "1345678")
+        assertThat(result).isFalse()
     }
 
     @Test
-    fun validatePassword_passwordsContainsDigit_returnTrue(){
-        val result = RegistrationUtil.validatePassword("abcdefg1", "abcdefg1")
-        assertThat(result).isTrue()
+    fun validatePassword_passwordsContainsDigit_returnFalse(){
+        val result = RegistrationUtil.validatePassword("abcdefge", "abcdefge")
+        assertThat(result).isFalse()
     }
 
     @Test
     fun validatePassword_passwordContainsCap_returnsFalse(){
         val result = RegistrationUtil.validatePassword("asdf1234", "asdf1234")
         assertThat(result).isFalse()
+    }
+
+    @Test
+    fun validatePassword_goodPassword_returnTrue(){
+        val result = RegistrationUtil.validatePassword("Abcd1234", "Abcd1234")
+        assertThat(result).isTrue()
+    }
+
+
+    @Test
+    fun validateEmail_emptyEmail_returnFalse(){
+        val result = RegistrationUtil.validateEmail("")
+        assertThat(result).isFalse()
+    }
+
+    @Test
+    fun validateEmail_repeatedEmail_returnFalse(){
+        val result = RegistrationUtil.validateEmail("connersmith1254@gmail.com")
+        assertThat(result).isFalse()
+    }
+
+    @Test
+    fun validateEmail_notAEmail_returnFalse(){
+        val result = RegistrationUtil.validateEmail("sad.comfiab@ds")
+        assertThat(result).isFalse()
+    }
+
+    @Test
+    fun validateEmail_goodEmail_returnTrue(){
+        val result = RegistrationUtil.validateEmail("ahhhhh@gmail.com")
+        assertThat(result).isTrue()
+    }
+
+
+    @Test
+    fun validateName_weirdName_returnFalse(){
+        val result = RegistrationUtil.validateName("wa938746wer9f")
+        assertThat(result).isFalse()
+    }
+
+    @Test
+    fun validateName_goodName_returnTrue() {
+        val result = RegistrationUtil.validateName("conner")
+        assertThat(result).isTrue()
+    }
+
+    @Test
+    fun validateUsername_emptyUsername_returnFalse(){
+        val result = RegistrationUtil.validateUsername("")
+        assertThat(result).isFalse()
+    }
+
+    @Test
+    fun validateUsername_tooLongUsername_returnFalse(){
+        val result = RegistrationUtil.validateUsername("asdygfuhaw8e7rt8qw49yerfqiweyfriqwefgiqwaefitwqei7tfiqwetgfiqtwefuqweitfiaweufgi")
+        assertThat(result).isFalse()
+    }
+
+    @Test
+    fun validateUsername_takenUsername_returnFalse(){
+        val result = RegistrationUtil.validateUsername("bigboi")
+        assertThat(result).isFalse()
+    }
+
+    @Test
+    fun validateUsername_forbiddenUsername_returnFalse(){
+        val result = RegistrationUtil.validateUsername("mushrooman")
+        assertThat(result).isFalse()
+    }
+
+    @Test
+    fun validateUsername_usernameHasName_returnFalse(){
+        val result = RegistrationUtil.validateUsername("connersmith")
+        assertThat(result).isFalse()
+    }
+
+    @Test
+    fun validateUsername_goodUsername_returnTrue(){
+        val result = RegistrationUtil.validateUsername("bingus")
+        assertThat(result).isTrue()
     }
 }
